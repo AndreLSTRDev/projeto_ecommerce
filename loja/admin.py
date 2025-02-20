f81417c82bd9140928119ec9b97731db058e864c
AndreLSTRDev <andre.profissional.dev@gmail.com>
2025-02-19 23:52:34 -0300

    Update admin.py acréscimo de comentários

--- loja/admin.py
+++ loja/admin.py
@@ -1,8 +1,17 @@
 
 
+from django.contrib import admin  # Importa o módulo de administração do Django
+from .models import Categoria, Produto  # Importa os modelos que serão registrados no painel administrativo
 
-from django.contrib import admin
-from .models import Categoria, Produto
+# Registra o modelo Categoria para que possa ser gerenciado pelo painel administrativo do Django
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome')  # Define os campos exibidos na lista de categorias
+    search_fields = ('nome',)  # Adiciona um campo de busca baseado no nome
 
-admin.site.register(Categoria)
-admin.site.register(Produto)
+# Registra o modelo Produto para gerenciamento no painel administrativo
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'preco', 'categoria')  # Define os campos exibidos na lista de produtos
+    search_fields = ('nome',)  # Permite buscar produtos pelo nome
+    list_filter = ('categoria',)  # Adiciona um filtro baseado na categoria dos produtos
